eaab1a4f3753d5ba496923b189bd496e0e56d21a
Cassandre Mauperrin <cassandre@pc44.home>
2018-04-22 18:43:26 +0200

    [NEON-INFRA-1] Add ngnix dockerfile, configuration and docker-compose => consul, nginx, registrator

---
+++ nginx/nginx-template.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+/usr/sbin/nginx -c /etc/nginx/nginx.conf && \
+consul-template -consul-addr=${CONSUL_AGENT_ADDR}:${CONSUL_AGENT_PORT} \
+                -template="./consul.ctmpl:/etc/nginx/conf.d/services.conf:/usr/sbin/nginx -s reload"
+
+# consul-template -consul-addr=${CONSUL_AGENT_ADDR}:${CONSUL_AGENT_PORT} \
+#                 -template="/etc/consul-templates/consul.ctmpl" -dry -once
